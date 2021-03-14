75eb8394c952c859af6fa215ccb200733c6bb231
theoboraud <theoboraud@outlook.com>
2021-03-14 15:37:09 +0100

    Update project on macOS

--- Assets/Scripts/Projectiles/Bullet.cs
+++ Assets/Scripts/Projectiles/Bullet.cs
@@ -4,11 +4,11 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
-    public GameObject hitEffect;
+    public GameObject HitEffect;
 
     void OnCollisionEnter2D(Collision2D _collision)
     {
-        GameObject _effect = Instantiate(hitEffect, transform.position, Quaternion.identity);
+        GameObject _effect = Instantiate(HitEffect, transform.position, Quaternion.identity);
         Destroy(_effect, _effect.GetComponent<Animator>().GetCurrentAnimatorStateInfo(0).length);
         Destroy(gameObject);
     }
